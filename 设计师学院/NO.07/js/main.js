877e8c58d6aac72d9b75f4ab689d6c95bf332f38
Aelousdp <806267856@qq.com>
2018-05-09 12:47:12 +0800

    No.7 - 使用 animate.css 实现一个优雅的登录框

--- 设计师学院/NO.07/js/main.js
+++ 设计师学院/NO.07/js/main.js
@@ -1,17 +1,20 @@
-let oLabel = $(".form__label");
-let oWrapper = $(".form__wrapper");
-let oInput = $(".form__input");
+$(document).ready(function () {     //等待DOM元素加载完毕
+    let oLabel = $(".form__label");
+    let oWrapper = $(".form__wrapper");
+    let oInput = $(".form__input");
 
-for (let i = 0; i < 2; i++) {
-    oInput.eq(i).focus(function () {
-        oLabel.eq(i).css("top", "-10px");
-        oLabel.eq(i).css("font-size", "15px");
-    });
-    oInput.eq(i).blur(function () {
-        let oVal = oInput.eq(i).val();
-        if (oVal !== null) {
-            oLabel.eq(i).css("top", "");
-            oLabel.eq(i).css("font-size", "");
-        }
-    });
-}
+    for (let i = 0; i < 2; i++) {
+        oInput.eq(i).focus(function () {
+            oLabel.eq(i).css("top", "-10px");
+            oLabel.eq(i).css("font-size", "15px");
+        });
+        oInput.eq(i).blur(function () {
+            let oVal = oInput.eq(i).val();
+            if (oVal !== null) {
+                oLabel.eq(i).css("top", "");
+                oLabel.eq(i).css("font-size", "");
+            }
+        });
+    }
+});
+
